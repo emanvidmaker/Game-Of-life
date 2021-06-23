3f4016715cc10d803188571698ad2e27c5fc6530
Emanuel Acosta <emanuelacosta809@gmail.com>
2021-06-23 02:08:06 -0400

    Parity with 2d-main

--- Assets/Scripts/GameOfLifeRenderer.cs
+++ Assets/Scripts/GameOfLifeRenderer.cs
@@ -23,35 +23,35 @@ public class GameOfLifeRenderer : MonoBehaviour
         {0,0,1,0,0,0,1,0,1,0,0,0,0,0,0,0},
         {1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0}
         };
-    List<int[,]> iterations;
-
+    public GameObject Prefab;
+    public int size = 16;
     GameOfLifeClass game; 
+    List<int[,]> iterations;
+    GameObject cells;
+    List<GameObject> objectPool;
     Camera Camera;
-    // Start is called before the first frame update
-    void Start()
-    {
-        int size = 16;
-        game = new GameOfLifeClass(RandomPatternGenerator(size,size));
+    int aliveCells = 0;
 
+    void Start()=>StartSimulation(RandomPatternGenerator(size,size));
+    void StartSimulation(int[,] inputGrid)
+    {
+        game = new GameOfLifeClass(inputGrid);
 
 
         objectPool = new List<GameObject>();
         iterations = new List<int[,]>(); 
         cells = new GameObject("cells");
+
         int width = game.gridPattern.GetLength(0);
         int hight = game.gridPattern.GetLength(1);
 
         Camera = Camera.main;
-        
         Camera.orthographicSize = Mathf.Sqrt(width*hight)*0.55f;
         Camera.transform.position = new Vector3(width/2,hight/2,Camera.transform.position.z);
-        // RenderGeneration(game.gridPattern);
         
         StartCoroutine("StartCycles");
     }
-      public GameObject Prefab;
-    GameObject cells;
-    List<GameObject> objectPool;
+    
     IEnumerator StartCycles(){
         for (int i = 0; i < 100; i++)
         {
@@ -62,8 +62,6 @@ public class GameOfLifeRenderer : MonoBehaviour
             yield return new WaitForSeconds(0.020f);
         }
     }
-    int aliveCells = 0;
-
     void RenderGeneration(int[,] grid, int Z = 0 , bool redraw = true){
         int width = grid.GetLength(0);
         int hight = grid.GetLength(1);
